975e25818843a0c7be26848270a842d78819565f
Luxmi Gupta <73395071+luxmi1799@users.noreply.github.com>
2022-07-02 13:41:09 +0530

    commit

--- lib/colors_code/colors_code.dart
+++ lib/colors_code/colors_code.dart
@@ -3,4 +3,8 @@ class colors_code {
   static String app_color = "CC0000";
   static String button_color = "07D3DF";
   static double _opacity = 0.9;
+  static String top_g = "FEB974";
+  static String mid_g = "944C1E";
+  static String bottom_g = "772F1A";
+  static String profile_g = "07D3DF";
 }
--- lib/myprofile.dart
+++ lib/myprofile.dart
@@ -42,7 +42,10 @@ class _myprofile extends State<myprofile> {
               ),
               actions: <Widget>[
                 IconButton(
-                  icon: Icon(Icons.search,color: Colors.black,),
+                  icon: ImageIcon(AssetImage("assets/search.png"),
+                    color: Colors.black,
+                      size: 28,
+                      ),
                   onPressed: () {
                   },
                 ),
@@ -54,6 +57,102 @@ class _myprofile extends State<myprofile> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
+
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            SizedBox(
+                                width: 30,
+                                height: 30,
+                                child: Image.asset("assets/profile_eyecircle.png")
+                            ),
+                            // Front image
+                            SizedBox(
+                                width: 16,
+                                height: 16,
+                                child: Image.asset("assets/edit.png")),
+                          ],
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20.0,right: 20),
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              SizedBox(
+                                  width: 100,
+                                  height: 100,
+                                  child: Image.asset("assets/halfcircle.png")),
+                              // Front image
+                              SizedBox(
+                                  width: 90,
+                                  height: 90,
+                                  child: Image.asset("assets/profilepic.png")),
+                            ],
+                          ),
+                        ),
+
+                        Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            SizedBox(
+                                width: 30,
+                                height: 30,
+                                child: Image.asset("assets/profile_eyecircle.png")),
+                            // Front image
+                            SizedBox(
+                                width: 16,
+                                height: 16,
+                                child: Image.asset("assets/ssetting.png")),
+                          ],
+                        ),
+
+                      ],
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0,bottom: 8),
+                    child: Center(
+                      child: Text("14788888",style: TextStyle(
+                        color: Color(0xffCC0000),
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold
+                      ),),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.only(top: 0.0,bottom: 8),
+                    child: Center(
+                      child: Text("ID:14788888",style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 13,
+                      ),),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.only(top: 0.0,bottom: 8),
+                    child: Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                           Image.asset("assets/india.png"),
+                          Text(" IN",style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 10,
+                          ),),
+                        ],
+                      )
+                    ),
+                  ),
+
 
                 ],
               ),
